279a963411406139f5fcfc95a784e9dffcb51a92
Izuorah Jeffrey <88318304+Jeffreywastaken@users.noreply.github.com>
2022-06-09 19:10:24 +0400

    forgot to add ceasar

---
+++ Arrays/ceasar.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    // Check program is run with one single line command prompt
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n"); //if commancd line not equals to 2
+        return 1;
+    }
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+        int key = atoi(argv[1]);   // Change argv from string to int
+
+        string plaintext = get_string("Plaintext: ");  //  Ask user for plaintext
+        printf("Ciphertext: ");
+
+        for (int j = 0; j < strlen(plaintext); j++)
+        {
+            if (isupper(plaintext[j]))
+            {
+                printf("%c", (plaintext[j] - 65 + key) % 26 + 65);
+            }
+            else if (islower(plaintext[j]))
+            {
+                printf("%c", (plaintext[j] - 97 + key) % 26 + 97);
+            }
+            else
+            {
+                printf("%c", plaintext[j]);
+            }
+        }
+        printf("\n");
+
+}
